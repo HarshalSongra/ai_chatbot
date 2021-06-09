7c1490d21ecff028e44a4538a20f8d1aa6e499a8
Harshal Songra <harshsongra0806@gmail.com>
2021-06-09 19:57:49 +0530

    data arrangement for training

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from nltk.stem.lancaster import LancasterStemmer
 
 stemmer = LancasterStemmer()
 
-import numpy
+import numpy as np
 import tflearn
 import tensorflow
 import random
@@ -32,3 +32,35 @@ for intent in data['intents']:
         if intent['tag'] not in labels:
             labels.append(intent['tag'])
 
+
+# arranging data
+words = [stemmer.stem(w.lower()) for w in words]
+words = sorted(list(set(words)))
+
+labels = sorted(labels)
+
+training = []
+output = []
+
+out_empty = [0 for _ in range(len(labels))]
+
+# saperating data for training and output
+for x , doc in enumerate(doc_x):
+    bag = []
+    wrds = [stemmer.stem(w) for w in doc]
+    
+    for w in words:
+        if w in wrds:
+            bag.append(1)
+        else:
+            bag.append(0)
+    
+    output_row = out_empty[:]
+    output_row[labels.index(doc_y[x])] = 1
+    
+    training.append(bag)
+    output.append(output_row)  
+
+
+training = np.array(training)
+output = np.array(output)
